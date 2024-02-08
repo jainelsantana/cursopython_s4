b600a2497285cdcd218fbb0a0eb7ca1074cc9015
jainelsantana <jainel@hotmail.com.br>
2024-02-07 21:56:25 -0300

    aula 36

--- aula36.py
+++ aula36.py
@@ -4,13 +4,20 @@
 # O Python conhece a pasta onde__main__ está e as pastas abaixo dele.
 # Ele não reconhece pastas e módulos acima do __main__ por padrão
 # O Python conhece todos os módulos e pacotes presentes nos caminhos sys.path
-try:
-    import sys
-    sys.path.append('/home/jainel/Desktop')
-except ModuleNotFoundError:
-    ...
+# try:
+#     import sys
+#    # sys.path.append('/home/jainel/Desktop')
+# except ModuleNotFoundError:
+#     ...
 import aula36_m
-import modulo_py
-print ('Esté módulo se chama', __name__)
+from aula36_m import variavel_modulo, soma
+# #import modulo_py
+# print ('Esté módulo se chama', __name__)
 
-print(*sys.path, sep='\n')
+# print(*sys.path, sep='\n')
+print('Este módulo se chama',__name__)
+print(aula36_m.variavel_modulo)
+print(variavel_modulo)
+print(soma(2, 3))
+print(aula36_m.soma(5, 3))
+
--- aula36_m.py
+++ aula36_m.py
@@ -1,1 +1,5 @@
-print ('Esté módulo se chama', __name__)
+#print ('Esté módulo se chama', __name__)
+variavel_modulo="jainel"
+
+def soma(x, y):
+    return x + y
